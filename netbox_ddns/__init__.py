cbd1f69980b1838a1d97581b4003a9f281e49081
Yvan E. Watchman <git@yvanwatchman.eu>
2024-06-11 13:53:38 +0200

    Add compatibility for Netbox 4.0
      - Fix template date filter
      - replace imports with the new correct ones
      - Remove bootstrapmixin
      - Update version requirements

--- netbox_ddns/__init__.py
+++ netbox_ddns/__init__.py
@@ -1,7 +1,7 @@
-VERSION = '1.2.10'
+VERSION = '1.3.0'
 
 try:
-    from extras.plugins import PluginConfig
+    from netbox.plugins import PluginConfig
 except ImportError:
     # Dummy for when importing outside of netbox
     class PluginConfig:
@@ -12,8 +12,8 @@ class NetBoxDDNSConfig(PluginConfig):
     name = 'netbox_ddns'
     verbose_name = 'Dynamic DNS'
     version = VERSION
-    min_version = '3.0.0'
-    max_version = '3.7.999'
+    min_version = '4.0.0'
+    max_version = '4.0.999'
     author = 'Sander Steffann'
     author_email = 'sander@steffann.nl'
     description = 'Dynamic DNS Connector for NetBox'
--- netbox_ddns/forms.py
+++ netbox_ddns/forms.py
@@ -1,10 +1,9 @@
 from django import forms
 
 from netbox_ddns.models import ExtraDNSName
-from utilities.forms import BootstrapMixin
 
 
-class ExtraDNSNameEditForm(BootstrapMixin, forms.ModelForm):
+class ExtraDNSNameEditForm(forms.ModelForm):
     class Meta:
         model = ExtraDNSName
         fields = ['name']
